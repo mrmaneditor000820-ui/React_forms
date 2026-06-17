3695e73b93c892f3252e3685fe6519a56fd67db4
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-17 06:06:47 +0500

    controll component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   const [name, setName] = useState("");
 
-export default App;
+//   function handlechange (e){
+//    setName(e.target.value)
+//   }
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={handlechange}
+//       />
+
+//       <h2>{name}</h2>
+//     </>
+//   );
+// }
+// export default App
+
+
