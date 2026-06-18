03ac7627de5e3ba0374e7de642ee541b04aea286
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-18 17:18:32 +0500

    Add form handling with name and email inputs

--- src/App.js
+++ src/App.js
@@ -51,3 +51,39 @@
 // preventDefault()
 
 // Browser ka default behavior (page refresh) rok deta hai.
+
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(name);
+    console.log(email);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default App
